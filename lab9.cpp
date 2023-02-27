98637f44402530c45da8e15ca4baead8e458e381
TonySavich <92541859+TonySavich@users.noreply.github.com>
2023-02-27 22:54:58 +0200

    Update lab9.cpp

--- lab9.cpp
+++ lab9.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <fstream>
 
 int const N = 5;
@@ -24,7 +24,7 @@ int main()
 
     }
     in.close();
-    
+
     int dano;
 
     std::cin >> dano;
@@ -40,44 +40,47 @@ int main()
     path[0][dano] = 0;
 
     int counter = 1;
-    int dopmas[N] , t;
+    int dopmas[N], t;
     t = 0;
+  
+        while (counter < N) {
 
-    while (counter < N) {
-
-        for (int i = 0; i < N; i++) {
-            path[counter][i] = path[counter - 1][i];
-            if (path[counter][i] < INT_MAX) {
-                dopmas[t] = i;
-                t++;
+            for (int i = 0; i < N; i++) {
+                path[counter][i] = path[counter - 1][i];
+                if (path[counter][i] < INT_MAX) {
+                    dopmas[t] = i;
+                    t++;
+                }
             }
-        }
-
-        
 
 
-        for (int j = 0; j < t; j++) {
-            for (int i = 0; i < N; i++) {          
+
+
+            for (int j = 0; j < t; j++) {
+                for (int i = 0; i < N; i++) {
                     if (mas[dopmas[j]][i] != 0) {
                         if (path[counter][i] > mas[dopmas[j]][i] + path[counter][dopmas[j]]) {
-                            path[counter][i] = mas[dopmas[j]][i] + path[dopmas[j]][dopmas[j]];
+                            path[counter][i] = mas[dopmas[j]][i] + path[counter][dopmas[j]];
+                            
 
-                        }                  
+                        }
+                    }
+
                 }
-
             }
+
+
+            counter++;
+
+
+            t = 0;
+
+
+
+
         }
 
-
-        counter++;
-
-
-        t = 0;
-
-
-
-
-    }
+    
     std::cout << std::endl;
     for (int i = 0; i < N; i++) {
         if (i == dano) {
@@ -89,6 +92,6 @@ int main()
         else {
             std::cout << i + 1 << " " << path[counter - 1][i] << std::endl;
         }
-    }
-
+    
+}
 }
